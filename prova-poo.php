27b05b9f627d168192b984323519222f47f15604
jvmantovas <vmsjoao@gmail.com>
2023-04-15 16:21:56 -0300

    refactor: object-oriented refactor

--- prova-poo.php
+++ prova-poo.php
@@ -1,48 +1,60 @@
 <?php
-//codigo antigo
-$pattern = readline("Digite 'Cruz' ou 'X': ");
+$patternName = readline("Digite 'Cruz' ou 'X': ");
 
-if (strtolower($pattern) == 'cruz') {
-    echo "Você escolheu desenhar: Cruz\n";
-    for ($row = 0; $row < 5; $row++) {
-        for ($col = 0; $col < 7; $col++) {
-            if ($row == 0 && $col == 3) {
-                echo "\e[31m*\e[0m";
-            } else if ($row == 1 && ($col == 1 || $col == 2 || $col == 3 || $col == 4 || $col == 5)) {
-                echo "\e[31m*\e[0m";
-            } else if ($row == 2 && $col == 3) {
-                echo "\e[31m*\e[0m";
-            } else if ($row == 3 && $col == 3) {
-                echo "\e[31m*\e[0m";
-            } else if ($row == 4 && $col == 3) {
-                echo "\e[31m*\e[0m";
-            } else {
-                echo ".";
-            }
-        }
-        echo "\n";
-    }
-} else if (strtolower($pattern) == 'x') {
-    echo "Você escolheu desenhar: X\n";
-    for ($row = 0; $row < 5; $row++) {
-        for ($col = 0; $col < 5; $col++) {
-            if ($row == 0 && ($col == 0 || $col == 4)) {
-                echo "\e[31m*\e[0m";
-            } else if ($row == 1 && ($col == 1 || $col == 3)) {
-                echo "\e[31m*\e[0m";
-            } else if ($row == 2 && $col == 2) {
-                echo "\e[31m*\e[0m";
-            } else if ($row == 3 && ($col == 1 || $col == 3)) {
-                echo "\e[31m*\e[0m";
-            } else if ($row == 4 && ($col == 0 || $col == 4)) {
-                echo "\e[31m*\e[0m";
-            } else {
-                echo ".";
-            }
-        }
-        echo "\n";
-    }
+echo "Você escolheu desenhar: \e[33m{$patternName}\e[0m\n";
+
+if (strtolower($patternName) == 'cruz') {
+    $pattern = new Cross();
+} else if (strtolower($patternName) == 'x') {
+    $pattern = new X();
 } else {
-    echo "Opção inválida!";
+    echo "Opção inválida!\n";
     exit();
 }
+
+abstract class Pattern
+{
+    abstract public function draw();
+    protected function printChar()
+    {
+        echo "\e[31m*\e[0m";
+    }
+}
+
+class Cross extends Pattern
+{
+    function draw()
+    {
+        for ($row = 0; $row < 5; $row++) {
+            for ($col = 0; $col < 7; $col++) {
+                $this->isCross($row, $col) ? $this->printChar() : print('.');
+            }
+            echo "\n";
+        }
+    }
+
+    function isCross($row, $col)
+    {
+        return $row == 1 || $col == 3;
+    }
+}
+
+class X extends Pattern
+{
+    function draw()
+    {
+        for ($row = 0; $row < 5; $row++) {
+            for ($col = 0; $col < 5; $col++) {
+                $this->isX($row, $col) ? $this->printChar() : print('.');
+            }
+            echo "\n";
+        }
+    }
+
+    function isX($row, $col)
+    {
+        return $row == $col || $row + $col == 5 - 1;
+    }
+}
+
+$pattern->draw();
