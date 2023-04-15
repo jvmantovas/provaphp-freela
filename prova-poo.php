711e4c8ecfe79f13d9dd7f613b307866e995ddbd
jvmantovas <vmsjoao@gmail.com>
2023-04-15 16:55:55 -0300

    feat: choose color to draw

--- prova-poo.php
+++ prova-poo.php
@@ -2,10 +2,10 @@
 
 abstract class Pattern
 {
-    abstract public function draw();
-    protected function printChar()
+    abstract public function draw(int $color);
+    protected function printChar(string $char, int $color)
     {
-        echo "\e[31m*\e[0m";
+        echo "\e[{$color}m{$char}\e[0m";
     }
 }
 
@@ -13,11 +13,11 @@ class Cross extends Pattern
 {
     private $rows = 5;
     private $cols = 7;
-    public function draw()
+    public function draw($color)
     {
         for ($row = 0; $row < $this->rows; $row++) {
             for ($col = 0; $col < $this->cols; $col++) {
-                $this->isCross($row, $col) ? $this->printChar() : print('.');
+                $this->isCross($row, $col) ? $this->printChar('*', $color) : print('.');
             }
             echo "\n";
         }
@@ -33,11 +33,11 @@ class X extends Pattern
 {
     private $rows = 5;
     private $cols = 5;
-    public function draw()
+    public function draw($color)
     {
         for ($row = 0; $row < $this->rows; $row++) {
             for ($col = 0; $col < $this->cols; $col++) {
-                $this->isX($row, $col) ? $this->printChar() : print('.');
+                $this->isX($row, $col) ? $this->printChar('*', $color) : print('.');
             }
             echo "\n";
         }
@@ -45,13 +45,15 @@ class X extends Pattern
 
     private function isX($row, $col)
     {
-        return $row == $col || $row + $col == 5 - 1;
+        return $row == $col || $row + $col == $this->rows - 1;
     }
 }
 
 while (true) {
     $patternName = readline("Digite 'Cruz' ou 'X': ");
-    echo "Você escolheu desenhar: \e[33m{$patternName}\e[0m\n";
+    echo "Agora escolha uma cor: \n\e[31mVermelho:\e[0m 31 || \e[32mVerde:\e[0m 32 || \e[33mAmarelo:\e[0m 33 || \e[34mAzul:\e[0m 34 || \e[35mRoxo:\e[0m 35 || \e[36mCiano:\e[0m 36 || \e[37mBranco:\e[0m 37\n";
+    $color = readline("Número da cor: ");
+    echo "Você escolheu desenhar: \e[{$color}m{$patternName}\e[0m\n";
 
     if (strtolower($patternName) == 'cruz') {
         $pattern = new Cross();
@@ -64,4 +66,4 @@ while (true) {
     }
 }
 
-$pattern->draw();
+$pattern->draw($color);
